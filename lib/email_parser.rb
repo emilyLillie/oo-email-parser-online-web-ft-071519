6117d8865932cce29b2d50f50dc0a01985c94c8e
Emily Lillie <emilylillie3@gmail.com>
2019-07-26 15:56:01 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -11,7 +11,7 @@ class EmailAddressParser
    end
 
     def parse 
-     array = @emails.split(/[ , ]/).uniq 
+     array = @emails.split(/[ , ]/).uniq #RegEx
      array.delete_if {|element| element.empty?} 
      array
    end
